75cf9e9506dd6570ee3545e5d90b4b920b7c58a4
salogist <salarkouchaki141@gmail.com>
2025-07-27 18:44:02 +0330

    new changes

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
-import React from "react";
 
 function Navifoot(){
+
+  
     return(
         <footer className="flex flex-row items-center justify-center pt-14 pb-7">
           <p className="text-sm text-gray-500">Copyright © 2025 FGB</p>
